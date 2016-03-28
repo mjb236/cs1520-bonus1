a7832592b94325099f5d1c4d9e9704206076ae00
mjb236 <mjb236@pitt.edu>
2016-03-28 12:23:09 -0400

    Correct initial file upload. Predictive search functioning.

--- bonus_one.js
+++ bonus_one.js
@@ -14,4 +14,70 @@
 (function() {
 	// Magic!
 	console.log('Keepin\'n it clean with an external script!');
+	
+	$.getJSON("http://www.mattbowytz.com/simple_api.json?data=all", function(data) {
+		console.log(data);
+		$(".results").hide();
+		$(".results").css("visibility", "hidden");
+		var searchItems = new Array();
+		var i = 0;
+		
+		for(i = 0; i < data.data.interests.length; i++) {
+			searchItems[i] = data.data.interests[i];
+		}
+		
+		for(i = 0; i < data.data.programming.length; i++) {
+			searchItems[data.data.interests.length + i] = data.data.programming[i];
+		}
+		searchItems.sort();	
+		console.log(searchItems);
+		
+		//listen for enter key and submit search if it is pressed
+		$("#mainForm").keypress(function(event) {
+			if(event.which == 13) {
+				event.preventDefault();
+				location.href = "http://www.google.com/search?q=" + $(".flexsearch-input").val();
+			}
+		});
+		
+		//listen for submit button press and submit search if it is pressed
+		$(".flexsearch-submit").click(function(event) {
+			event.preventDefault();
+			window.location.href = "http://www.google.com/search?q=" + $(".flexsearch-input").val();
+			
+		});
+		
+		//on keyup, display possible search terms fround from the API
+		$(".flexsearch-input").keyup(function(event) {
+			$(".results").empty();
+			var input = $(".flexsearch-input").val();
+			var j = 0;
+			var resultList = new Array();
+			
+			if(input.length > 0) {
+				for(i = 0; i < searchItems.length; i++) {
+					if(searchItems[i].substring(0, input.length).toLowerCase() == input.toLowerCase()) {
+						resultList[j] = searchItems[i];
+						j++;
+					}
+				}
+			}
+		
+			if(resultList.length > 0) {
+				$(".results").show();
+				$(".results").css("visibility", "visible");
+				for(j = 0; j < resultList.length; j++) {
+					var link = "<li class=\"search-item\"><a href=\"http://www.google.com/search?q=" + 
+											resultList[j].toLowerCase() + "\">" + resultList[j].toLowerCase() + "</a></li>";
+					$(".results").append(link);
+				}
+			}
+			else {
+				$(".results").hide();
+			}
+						
+			console.log(input.length);
+		});
+
+	});
 })();
